55e364832fcd0ad2fd4a06af7919e4b9890f38a1
cyclespace <stackecommerce@gmail.com>
2025-08-08 13:45:56 +0200

    clerk datase

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,3 +1,4 @@
+import prisma from '@/lib/prisma'
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
 
@@ -5,12 +6,30 @@ export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data
-    const eventType = evt.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', evt.data)
+    // // Do something with payload
+    // // For this guide, log payload to console
+    // const { id } = evt.data
+    // const eventType = evt.type
+    // console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    // console.log('Webhook payload:', evt.data)
+
+    if (evt.type === 'user.created') {
+      const { id, email_addresses, first_name, image_url } = evt.data
+      try {
+        const newUser = await prisma.user.create({
+          data: {
+            clerkUserId: id,
+            email: email_addresses[0].email_address,
+            name: first_name,
+            imageUrl: image_url
+          }
+        });
+        return new Response(JSON.stringify(newUser), { status: 201 })
+      } catch(error: any) {
+        console.error('Error creating user:', error)
+        return new Response(JSON.stringify(error), { status: 500 })
+      }
+    }
 
     return new Response('Webhook received', { status: 200 })
   } catch (err) {
