5b4e94a0a08b40b93e52a65d862d8e7982dd87cc
cyclespace <stackecommerce@gmail.com>
2025-08-17 22:44:26 +0200

    sanity config fix

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,20 +2,19 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure' // Keep this import
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { structure } from './structure'
 
 export default defineConfig({
   name: 'default',
   title: 'news-articles',
-
   projectId: '2ezfpspe',
   dataset: 'production',
-
-  // Add structureTool to plugins array
   plugins: [
-    structureTool(),
+    structureTool({
+      structure
+    }),
     visionTool()
   ],
-
   schema: {
     types: schemaTypes,
   },
